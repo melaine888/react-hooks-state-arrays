d8d1377515fab5b3b62a9c4b71fd180916e1c13a
Melaine Kanana <melaine.kanana@student.moringaschool.com>
2023-05-05 04:44:56 +0300

    DONE WITH ASSIGNMENT

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -10,7 +10,7 @@ function SpicyFoodList() {
   }
 
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id} onClick={() => handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
@@ -21,6 +21,17 @@ function SpicyFoodList() {
       <ul>{foodList}</ul>
     </div>
   );
+
+  function handleAddFood() {
+  const newFood = getNewSpicyFood();
+  const newFoodArray = [...foods, newFood];
+  setFoods(newFoodArray);
+
+  function handleLiClick(id) {
+    const newFoodArray = foods.filter((food) => food.id !== id);
+    setFoods(newFoodArray);
+  }
+}
 }
 
 export default SpicyFoodList;
